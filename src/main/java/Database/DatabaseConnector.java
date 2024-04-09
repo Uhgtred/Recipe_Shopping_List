ae9c3c561fa782fc9b28f6a9823a5f0f8c060e2d
Markus Kösters <Markus_koesters@gmx.de>
2024-04-09 10:17:03 +0200

    trying things with h2_database

--- src/main/java/Database/DatabaseConnector.java
+++ src/main/java/Database/DatabaseConnector.java
@@ -7,11 +7,22 @@ import java.sql.SQLException;
 public class DatabaseConnector implements DatabaseInterface{
 
     public static void main(String[] args) {
-        Connection connection =  connectToDatabase("jdbc:mysql:localhost/test?", "minty", "greatsqldb");
+        String dataBasePath = "mem:";
+        Connection connection =  connectToDatabase(("jdbc:h2:"+ dataBasePath));
+    }
+
+    public static Connection connectToDatabase(String url) {
+        try {
+            Connection connection = DriverManager.getConnection(url);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static Connection connectToDatabase(String url, String user, String password) {
-        try (Connection connection = DriverManager.getConnection(url, user, password)){
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
             return connection;
         } catch (SQLException e) {
             throw new RuntimeException(e);
