14050161e05abeb22737b97b66122d87f6918a54
uhgtred <markus_koesters@gmx.de>
2025-09-06 22:09:58 +0200

    Remove buildToolsVersion from app/build.gradle.kts and disable Gradle SDK auto-download in gradle.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 android {
     namespace = "com.example.recipe_shopping_list"
     compileSdk = 36
-    buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "com.example.recipe_shopping_list"
