c116a492da7ca1492cf7849ec73d2e0a2b48cc82
uhgtred <markus_koesters@gmx.de>
2025-09-07 21:51:26 +0200

    Remove `proguard-rules.pro` from `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,6 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                getDefaultProguardFile("proguard-rules.pro")
             )
         }
     }
